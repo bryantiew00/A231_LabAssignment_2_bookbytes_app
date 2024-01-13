c50215a9188b098b6f18652c816cdfbe8bd8ebee
Bryan Tiew <109888901+bryantiew00@users.noreply.github.com>
2024-01-14 04:36:16 +0800

    Update my_server_config.dart

--- lib/classPages/my_server_config.dart
+++ lib/classPages/my_server_config.dart
@@ -1,3 +1,3 @@
 class MyServerConfig{
-  static const String server = "http://10.19.28.75/bookbytes/server/php/";
+  static const String server = "http://10.19.28.75/A231_LabAssignment_2_bookbytes_app/server/php/";
 }
